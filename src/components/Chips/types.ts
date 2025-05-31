57e75d94a040f2a4d3552655153f8fb48c7282db
Sito <sito8943@gmail.com>
2025-05-31 11:44:04 -0400

    responsive

--- src/components/Chips/types.ts
+++ src/components/Chips/types.ts
@@ -2,6 +2,6 @@ export interface ChipPropsType {
     text: string
 }
 
-export interface LinkChipPropsType extends ChipPropsType {
+export interface LinkChipPropsType extends ChipPropsType, HTMLAnchorElement {
     href: string
 }
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -13,11 +13,11 @@ export const Home = () => {
     return (
         <main>
             <section id="hero" className="flex flex-col gap-5 mb-12 text-center">
-                <h1 className="text-6xl text-white font-bold">{t("_pages:home.title")}</h1>
+                <h1>{t("_pages:home.title")}</h1>
                 <p className="text-gray-400 text-base">{t("_pages:home.subtitle")}</p>
             </section>
             <section id="meta">
-                <ul className="grid gap-5 lg:grid-cols-3 md:grid-cols-2 items-stretch">
+                <ul className="grid gap-5 xl:grid-cols-3 md:grid-cols-2 items-stretch">
                     {data.meta.map((meta) => <li key={meta.name}>
                         <Card {...meta} />
                     </li>)}
