4fef0be5aa52cf447a5b12b089512197aa20bb5e
Sito <sito8943@gmail.com>
2025-05-31 11:34:58 -0400

    home with cards

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,10 @@
 import {useTranslation} from "react-i18next";
 
-// components
-import {LinkChip} from "../components";
+// data
+import data from "../data.json"
 
-const categories = ["models", "tools", "technique", "coming"];
+// components
+import {Card} from "../components";
 
 export const Home = () => {
 
@@ -11,15 +12,17 @@ export const Home = () => {
 
     return (
         <main>
-            <h1 className="text-6xl text-white">{t("_pages:home.title")}</h1>
-            <p className="text-white text-base">{t("_pages:home.subtitle")}</p>
-            <ul className="flex flex-wrap gap-4">
-                {categories.map((category) => (
-                    <li key={category}>
-                        <LinkChip href={`#${category}`} text={t(`_pages:home.categories.${category}`)}/>
-                    </li>
-                ))}
-            </ul>
+            <section id="hero" className="flex flex-col gap-5 mb-12 text-center">
+                <h1 className="text-6xl text-white font-bold">{t("_pages:home.title")}</h1>
+                <p className="text-gray-400 text-base">{t("_pages:home.subtitle")}</p>
+            </section>
+            <section id="meta">
+                <ul className="grid gap-5 lg:grid-cols-3 md:grid-cols-2 items-stretch">
+                    {data.meta.map((meta) => <li key={meta.name}>
+                        <Card {...meta} />
+                    </li>)}
+                </ul>
+            </section>
         </main>
     );
 };
