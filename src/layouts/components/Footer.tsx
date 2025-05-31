a7f2e459fb751133c6c3c8d169322090e925cc8c
Sito <sito8943@gmail.com>
2025-05-31 11:33:41 -0400

    default view

--- src/layouts/components/Footer.tsx
+++ src/layouts/components/Footer.tsx
@@ -1,5 +1,5 @@
 export const Footer = () => {
     return (
-        <></>
+        <footer></footer>
     );
 };
--- src/layouts/components/Navbar.tsx
+++ src/layouts/components/Navbar.tsx
@@ -1,5 +1,19 @@
+import {useTranslation} from "react-i18next";
+import {Link} from "react-router";
+
 export const Navbar = () => {
+    const {t} = useTranslation()
+
     return (
-        <></>
+        <header className="absolute">
+            <div>
+                <Link to="/">
+                    {t("_pages:app.name")}
+                </Link>
+            </div>
+            <nav>
+
+            </nav>
+        </header>
     );
 };
