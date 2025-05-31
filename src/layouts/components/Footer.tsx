d43d7019ae1c374c0dc193c33415d29e117f9238
Sito <sito8943@gmail.com>
2025-05-31 11:47:21 -0400

    footer

--- src/layouts/components/Footer.tsx
+++ src/layouts/components/Footer.tsx
@@ -1,5 +1,15 @@
+import {useTranslation} from "react-i18next";
+
+const date = new Date()
+
 export const Footer = () => {
+    const {t} = useTranslation()
+
     return (
-        <footer></footer>
+        <footer className="flex w-full items-center justify-center py-5 bg-dark">
+
+            <p className="text-gray-400">{t("_pages:footer.copyright")} {date.getFullYear()}</p>
+
+        </footer>
     );
 };
